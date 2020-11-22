641f90b7c3bab6e7c365f3fc9c8c863d1137f5a1
mika1407 <koneautomaatio@hotmail.com>
2020-11-22 14:49:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bird = document.querySelector('.bird')
+  const gameDisplay = document.querySelector('.game-container')
+  const ground = document.querySelector('.ground')
+
+  let birdLeft = 220
+  let birdBottom = 100
+  let gravity = 2
+
+
+  function startGame() {
+    birdBottom -= gravity
+    bird.style.bottom = birdBottom + 'px'
+    bird.style.left = birdLeft + 'px'
+  }
+  let timerId = setInterval(startGame, 20)
+
+  function control(e) {
+    if (e.keyCode === 32) {
+      jump()
+    }
+  }
+
+  function jump() {
+    if (birdBottom < 500) birdBottom += 50
+    bird.style.bottom = birdBottom + 'px'
+    console.log(birdBottom)
+  }
+  document.addEventListener('keyup', control)
+
+  function generateObstacle() {
+    let obstacleLeft = 500
+    let randomHeight = Math.random() * 60
+    let obstacleBottom = randomHeight
+    const obstacle = document.createElement('div')
+    obstacle.classList.add('obstacle')
+    gameDisplay.appendChild(obstacle)
+    obstacle.style.left = obstacleLeft + 'px'
+    obstacle.style.bottom = obstacleBottom + 'px'
+
+    function moveObstacle() {
+        obstacleLeft -=2
+        obstacle.style.left = obstacleLeft + 'px'
+    }
+    let timerId = setInterval(moveObstacle, 20)
+
+  }
+  generateObstacle()
+})
